551d31d470cc2c10d11d6b94d54df6207c895530
httung12303 <blackzerob562@gmail.com>
2023-04-19 23:52:05 +0700

    Listen to multiple messages (Small quantity only)

--- src/main/java/xmpp/client/ClientReceiveThread.java
+++ src/main/java/xmpp/client/ClientReceiveThread.java
@@ -1,7 +1,6 @@
 package xmpp.client;
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -9,21 +8,18 @@ import java.nio.charset.StandardCharsets;
 public class ClientReceiveThread extends Thread {
     private Socket clientSocket;
     private DataInputStream input;
-    private DataOutputStream output;
 
-    public ClientReceiveThread(Socket clientSocket, DataInputStream input, DataOutputStream output) {
+    public ClientReceiveThread(Socket clientSocket) throws IOException {
         this.clientSocket = clientSocket;
-        this.input = input;
-        this.output = output;
+        this.input = new DataInputStream(clientSocket.getInputStream());
     }
 
     // We'll change this to return Stanza later on.
     public String receiveMessage() throws IOException {
         String result = "";
-        long remainingLen = input.readLong();
+        long remainingLen = input.readInt();
         int bytes;
         byte[] buffer = new byte[4 * 1024];
-        // Remember to synchronize when writing to this socket via its input stream.(later in server)
         while (remainingLen > 0 && (bytes = input.read(buffer, 0, (int) Math.min(remainingLen, buffer.length))) != -1) {
             remainingLen -= bytes;
             String temp = new String(buffer, 0, bytes, StandardCharsets.UTF_8);
@@ -40,9 +36,6 @@ public class ClientReceiveThread extends Thread {
         if(input != null) {
             input.close();
         }
-        if (output != null) {
-            output.close();
-        }
     }
 
     @Override
@@ -54,8 +47,7 @@ public class ClientReceiveThread extends Thread {
                     break;
                 }
                 receiveMessage();
-                // We close conn here for testing.
-                closeConn();
+                // Do something
             } catch (IOException e) {
                 try {
                     closeConn();
@@ -64,10 +56,5 @@ public class ClientReceiveThread extends Thread {
                 }
             }
         }
-    }
-
-    public static void main(String[] args) {
-        byte[] b = {65, 97, 95, 95};
-        System.out.println(new String(b, StandardCharsets.UTF_8));
     }
 }
