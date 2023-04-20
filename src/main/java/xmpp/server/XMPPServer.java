e0175b1a4031083af3f16b22f8214be360c40eb4
httung12303 <blackzerob562@gmail.com>
2023-04-20 08:15:52 +0700

    Add comments

--- src/main/java/xmpp/server/XMPPServer.java
+++ src/main/java/xmpp/server/XMPPServer.java
@@ -1,5 +1,7 @@
 package xmpp.server;
 
+import socketwrapper.SocketWrapper;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,31 +14,27 @@ public class XMPPServer {
     public Socket acceptConn() throws IOException {
         return serverSocket.accept();
     }
-    public void sendMessage(ConnSocketWrapper wrapper, String message) throws IOException {
+    public void sendMessage(SocketWrapper wrapper, String message) throws IOException {
         Thread thread = new ServerSendThread(wrapper, message);
         thread.start();
     }
     public static void sendExample() {
+        // The real implementation is similar except the loop does not do the sending
+        // but starts a receiving thread immediately.
+        // Since each connection listen on a separate DataInputStream, synchronization is not a problem, not foe the
+        // receiving side at least.
         try {
             XMPPServer server = new XMPPServer(10000);
             while (true) {
                 Socket connSocket = server.acceptConn();
-                ConnSocketWrapper connSocketWrapper = new ConnSocketWrapper(connSocket);
-                server.sendMessage(connSocketWrapper, "Hello alibabababababababababađâsd alibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsd");
-//                Thread.sleep(1);
-                server.sendMessage(connSocketWrapper, "HI alibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsd");
-//                Thread.sleep(1);
-
-                server.sendMessage(connSocketWrapper, "Xin chao alibabababababababababađâsd");
-//                Thread.sleep(1);
-
-                server.sendMessage(connSocketWrapper, "Bonjour alibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsd");
-//                Thread.sleep(1000);
-
-                server.sendMessage(connSocketWrapper, "Si alibabababababababababađâsd");
-//                Thread.sleep(1000);
-
-                server.sendMessage(connSocketWrapper, "Escobar alibabababababababababađâsd");
+                SocketWrapper socketWrapper = new SocketWrapper(connSocket);
+                // Start a receive thread, which listens to messages and interact with db based on the messages' content.
+                server.sendMessage(socketWrapper, "Hello alibabababababababababađâsd alibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsd");
+                server.sendMessage(socketWrapper, "HI alibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsd");
+                server.sendMessage(socketWrapper, "Xin chao alibabababababababababađâsd");
+                server.sendMessage(socketWrapper, "Bonjour alibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsdalibabababababababababađâsd");
+                server.sendMessage(socketWrapper, "Si alibabababababababababađâsd");
+                server.sendMessage(socketWrapper, "Escobar alibabababababababababađâsd");
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
