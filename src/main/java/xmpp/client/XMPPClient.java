2a69c8ca7e225e4e6fcf033908144fb7f8af088c
httung12303 <blackzerob562@gmail.com>
2023-04-20 14:54:13 +0700

    Add examples

--- src/main/java/xmpp/client/XMPPClient.java
+++ src/main/java/xmpp/client/XMPPClient.java
@@ -11,15 +11,50 @@ public class XMPPClient {
 
     public XMPPClient(String ip, int port) throws IOException {
         clientSocket = new SocketWrapper(new Socket(ip, port));
-        receiveThread = new ClientReceiveThread(clientSocket);
+    }
+
+    public void startReceiving() throws IOException {
+        receiveThread = new ClientReceiveThread(getClientSocket());
         receiveThread.start();
     }
 
-    public static void main(String[] args) {
+    public void sendMessage(String message) throws IOException {
+        Thread thread = new ClientSendThread(clientSocket, message);
+        thread.start();
+    }
+
+    public static void sendExample() {
         try {
             XMPPClient client = new XMPPClient("127.0.0.1", 10000);
+            client.sendMessage("abcascsadsddasdasdasdsdad");
+            client.sendMessage("abcascsadsddasdasdasdsdad1");
+            client.sendMessage("abcascsadsddasdasdasdsdad2");
+            client.sendMessage("abcascsadsddasdasdasdsdad3");
+            client.sendMessage("abcascsadsddasdasdasdsdad5");
+            client.sendMessage("abcascsadsddasdasdasdsdad6");
+            client.sendMessage("abcascsadsddasdasdasdsdad7");
+            client.sendMessage("abcascsadsddasdasdasdsdad8");
+            client.sendMessage("abcascsadsddasdasdasdsdad9");
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static void receiveExample() {
+        try {
+            XMPPClient client = new XMPPClient("127.0.0.1", 10000);
+            client.startReceiving();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        XMPPClient.sendExample();
+    }
+
+    public SocketWrapper getClientSocket() {
+        return clientSocket;
     }
 }
