c76981b24a99dd47657eb9f37810e0ff45b0db85
httung12303 <blackzerob562@gmail.com>
2023-04-25 15:40:28 +0700

    Change send example

--- src/main/java/xmpp/client/XMPPClient.java
+++ src/main/java/xmpp/client/XMPPClient.java
@@ -1,47 +1,60 @@
 package xmpp.client;
 
 import socketwrapper.SocketWrapper;
+import stanza.ResultIQ;
 import stanza.Stanza;
 
 import java.io.IOException;
 import java.net.*;
 
 public class XMPPClient {
-    private SocketWrapper clientSocket;
-    private Thread receiveThread;
+  private SocketWrapper clientSocket;
+  private Thread receiveThread;
 
-    public XMPPClient(String ip, int port) throws IOException {
-        clientSocket = new SocketWrapper(new Socket(ip, port));
+  public XMPPClient(String ip, int port) throws IOException {
+    clientSocket = new SocketWrapper(new Socket(ip, port));
+  }
+
+  public void startReceive() throws IOException {
+    receiveThread = new ClientReceiveThread(getClientSocket());
+    receiveThread.start();
+  }
+
+  public void sendStanza(Stanza stanza) throws IOException {
+    Thread thread = new ClientSendThread(clientSocket, stanza);
+    thread.start();
+  }
+
+  public static void sendExample() {
+    try {
+      XMPPClient client = new XMPPClient("127.0.0.1", 10000);
+      ResultIQ iq1 = new ResultIQ("client", "server", "1234");
+      iq1.addItem("temp", "30");
+      iq1.addItem("humid", "40");
+      client.sendStanza(iq1);
+      client.sendStanza(iq1);
+      client.sendStanza(iq1);
+      client.sendStanza(iq1);
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
     }
+  }
 
-    public void startReceive() throws IOException {
-        receiveThread = new ClientReceiveThread(getClientSocket());
-        receiveThread.start();
+  public static void receiveExample() {
+    try {
+      XMPPClient client = new XMPPClient("127.0.0.1", 10000);
+      client.startReceive();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
     }
+  }
 
-    public void sendStanza(Stanza stanza) throws IOException {
-        Thread thread = new ClientSendThread(clientSocket, stanza);
-        thread.start();
-    }
+  public static void main(String[] args) {
+    XMPPClient.sendExample();
+  }
 
-    public static void sendExample() {
-
-    }
-
-    public static void receiveExample() {
-        try {
-            XMPPClient client = new XMPPClient("127.0.0.1", 10000);
-            client.startReceive();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public static void main(String[] args) {
-        XMPPClient.sendExample();
-    }
-
-    public SocketWrapper getClientSocket() {
-        return clientSocket;
-    }
+  public SocketWrapper getClientSocket() {
+    return clientSocket;
+  }
 }
