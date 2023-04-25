0c369e459c27db26afc95eb8ab9dc94f1620faaf
httung12303 <blackzerob562@gmail.com>
2023-04-25 16:27:15 +0700

    Add example

--- src/main/java/xmpp/server/XMPPServer.java
+++ src/main/java/xmpp/server/XMPPServer.java
@@ -1,5 +1,6 @@
 package xmpp.server;
 
+import stanza.ResultIQ;
 import stanza.Stanza;
 import socketwrapper.SocketWrapper;
 
@@ -9,16 +10,20 @@ import java.net.Socket;
 
 public class XMPPServer {
     private final ServerSocket serverSocket;
+
     public XMPPServer(int port) throws IOException {
         serverSocket = new ServerSocket(port);
     }
+
     public Socket acceptConn() throws IOException {
         return serverSocket.accept();
     }
+
     public void sendStanza(SocketWrapper wrapper, Stanza stanza) throws IOException {
         Thread thread = new ServerSendThread(wrapper, stanza);
         thread.start();
     }
+
     public static void sendExample() {
         // The real implementation is similar except the loop does not do the sending
         // but starts a receiving thread immediately.
@@ -29,9 +34,18 @@ public class XMPPServer {
             while (true) {
                 Socket connSocket = server.acceptConn();
                 SocketWrapper socketWrapper = new SocketWrapper(connSocket);
+                ResultIQ iq1 = new ResultIQ("client", "server", "1234");
+                iq1.addItem("temp", "30");
+                iq1.addItem("humid", "40");
+                server.sendStanza(socketWrapper, iq1);
+                server.sendStanza(socketWrapper, iq1);
+                server.sendStanza(socketWrapper, iq1);
+                server.sendStanza(socketWrapper, iq1);
+                server.sendStanza(socketWrapper, iq1);
+                server.sendStanza(socketWrapper, iq1);
 
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
@@ -39,7 +53,7 @@ public class XMPPServer {
     public static void receiveExample() {
         try {
             XMPPServer server = new XMPPServer(10000);
-            while(true) {
+            while (true) {
                 Socket connSocket = server.acceptConn();
                 SocketWrapper socketWrapper = new SocketWrapper(connSocket);
                 new ServerReceiveThread(socketWrapper).start();
@@ -50,6 +64,7 @@ public class XMPPServer {
     }
 
     public static void main(String[] args) {
-        XMPPServer.receiveExample();
+        // XMPPServer.receiveExample();
+        XMPPServer.sendExample();
     }
 }
