c55ca1ea2e58d39bb38f7753ec2a609fc936ef65
httung12303 <blackzerob562@gmail.com>
2023-04-27 22:49:19 +0700

    Refactor code

--- src/main/java/environment/Environment.java
+++ src/main/java/environment/Environment.java
@@ -17,16 +17,24 @@ public class Environment {
         addedTime = LocalTime.of(hour, min, sec);
         temperature = rand.nextFloat() * 45 -5;
         humidity = rand.nextInt(80);
-        brightness = rand.nextInt(800);
+        brightness = rand.nextInt(5000);
     }
-    public void changeTemperature(boolean increase) {
-        temperature += (increase ? 1 : -1) * 1.5;
+    public void update(float temperature, int humidity, int brightness) {
+        changeTemperature(temperature);
+        changeHumidity(humidity);
+        changeBrightness(humidity);
     }
-    public void changeHumidity(boolean increase) {
-        humidity += (increase ? 1 : -1) * 2;
+    public void changeTemperature(float temperature) {
+        float changeRate = Math.min(1.5F, Math.abs(this.temperature - temperature));
+        this.temperature += (this.temperature > temperature ? -1 : 1) * changeRate;
     }
-    public void changeBrightness(boolean increase) {
-        brightness += (increase ? 1 : -1) * 20;
+    public void changeHumidity(int humidity) {
+        int changeRate = Math.min(2, Math.abs(this.humidity - humidity));
+        this.humidity += (this.humidity > humidity ? -1 : 1) * changeRate;
+    }
+    public void changeBrightness(int brightness) {
+        int changeRate = Math.min(100, Math.abs(this.brightness - brightness));
+        this.brightness += (this.brightness > brightness ? -1 : 1) * changeRate;
     }
 
     public float getTemperature() {
@@ -46,14 +54,5 @@ public class Environment {
         LocalTime result = now.plusHours(addedTime.getHour()).plusMinutes(addedTime.getMinute()).plusSeconds(addedTime.getSecond());
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         return result.format(formatter);
-    }
-
-    public static void main(String[] args) {
-        Random rand = new Random();
-        int hour = rand.nextInt(24);
-        int min = rand.nextInt(60);
-        int sec = rand.nextInt(60);
-        LocalTime time = LocalTime.of(hour, min, sec);
-        System.out.println(time.toString());
     }
 }
